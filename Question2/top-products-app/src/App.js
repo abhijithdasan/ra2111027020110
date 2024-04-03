48d0d38cecb78dcba5abe90a4ac0e0e93ac85e0d
Abhijith Dasan <abhijithdasan05@gmail.com>
2024-04-03 20:38:47 +0530

    Fixed error

--- Question2/top-products-app/src/App.js
+++ Question2/top-products-app/src/App.js
@@ -1,8 +1,33 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AllProducts from './pages/AllProducts';
-import ProductDetails from './pages/ProductDetails';
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+const AllProducts = () => (
+  <div>
+    <h1>All Products</h1>
+    <Card>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          Product 1
+        </Typography>
+        <Typography color="textSecondary">
+          Company: Company A
+        </Typography>
+        {/* Other product details */}
+      </CardContent>
+    </Card>
+    {/* Add more product cards as needed */}
+  </div>
+);
+
+const ProductDetails = ({ match }) => (
+  <div>
+    <h1>Product Details</h1>
+    <p>Product ID: {match.params.id}</p>
+    {/* Fetch product details based on ID */}
+  </div>
+);
 
 function App() {
   return (
