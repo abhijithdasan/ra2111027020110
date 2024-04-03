54022151de4e4ce889c07b4a5a769657ba55528d
Abhijith Dasan <abhijithdasan05@gmail.com>
2024-04-03 20:53:50 +0530

    last commit

--- Question2/top-products-app/src/index.js
+++ Question2/top-products-app/src/index.js
@@ -1,14 +1,17 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import { Card, CardContent, Typography } from '@material-ui/core'; // Import Material-UI components
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Button, Typography, Container } from '@material-ui/core'; // Import Material-UI components
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App Card={Card} CardContent={CardContent} Typography={Typography} /> {/* Pass Material-UI components as props */}
+      <App 
+        ButtonComponent={Button} 
+        TypographyComponent={Typography} 
+        ContainerComponent={Container} 
+      />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
